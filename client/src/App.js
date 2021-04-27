d6c0c23f79bee6b01cb1eee87b3a3e96ccd40956
Dmitriy Vorozheykin <lmidorunl@gmail.com>
2021-04-27 15:33:05 +0400

    feat: add login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+import React, { useState } from 'react'
+
 import Login from "./Login";
 
 
 function App() {
+  const [id, setid] = useState()
+
   return (
-    <Login />
+    <>
+      <Login onSubmit={setid} />
+    </>
   );
 }
 
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Container, Form, Button } from 'react-bootstrap'
+import { v4 as uuidv4 } from 'uuid'
 
+export default function Login({ onSubmit }) {
+  const idInput = useRef()
 
-export default function Login() {
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    onSubmit(idInput.current.value)
+  }
+
+  const generateNewId = () => {
+    idInput.current.value = uuidv4()
+  }
+
   return (
     <Container
       className='d-flex align-items-center'
@@ -10,13 +21,16 @@ export default function Login() {
         height: `100vh`
       }}
     >
-      <Form>
+      <Form
+        onSubmit={handleFormSubmit}
+        className='w-100'
+      >
         <Form.Group>
           <Form.Label>Ваш айди</Form.Label>
-          <Form.Control type='text' placeholder='Введите ваш айди в это поле' />
+          <Form.Control ref={idInput} type='text' placeholder='Введите ваш айди в это поле' />
         </Form.Group>
-        <Button variant='primary' className='mr-2'>Войти</Button>
-        <Button variant='secondary'>Сгенерировать новый айди</Button>
+        <Button type='submit' className='mr-2'>Войти</Button>
+        <Button onClick={generateNewId} variant='secondary'>Сгенерировать новый айди</Button>
       </Form >
     </Container>
   )
